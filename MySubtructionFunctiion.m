3d99fb1dffe32f4910f2f33ee3ec8fb30656f721
HabteMan27 <106169121+HabteMan27@users.noreply.github.com>
2022-05-27 06:27:38 +0300

    Comment is added to addition function and subtruction function is created as a new file

---
+++ MySubtructionFunctiion.m
@@ -0,0 +1,4 @@
+function [z] = MySubtructionFunctiion(x,y)
+  % this funtion subtructs two numbers
+  % this comment is added for testing purpose
+  z = x - y;
--- additionFunction.m
+++ additionFunction.m
@@ -1,4 +1,6 @@
 function [z] = additionFunction(x,y)
   % this funtion adds two numbers
   % this comment is added for testing purpose
+  % change 2
+  
   z = x + y;
